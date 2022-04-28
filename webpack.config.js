95dbb46f558156a6ed6213b4ddba7fe61cfeed3c
Larrire <linekerlarrire@gmail.com>
2022-04-28 08:09:17 -0300

    Configurando regra no webpack para arquivos .scss

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader']
+      },{
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   }
