d4988902b4a802a55f8f443e75c4252915f21cb2
Larrire <linekerlarrire@gmail.com>
2022-04-28 07:49:49 -0300

    Configurando regra no webpack para arquivos .css

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },{
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
