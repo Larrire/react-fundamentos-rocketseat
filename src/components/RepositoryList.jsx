5da0850e51e6ddd89f8af31e1f3b62d3bd8fc8e3
Larrire <linekerlarrire@gmail.com>
2022-05-12 08:18:58 -0300

    Puxando dados do github e criando paginação dos repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,38 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+import { RepositoryPagination } from "./RepositoryPagination";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+  const [activePage, setActivePage] = useState(1);
+  const repos_per_page = 5
 
-  const reposiriesArray = [
-    {name: 'title', description: 'description', link: 'https://google.com'},
-    {name: 'title 2', description: 'description 2', link: 'https://google.com'},
-    { description: 'description 3', link: 'https://google.com'},
-    {name: 'title 4', link: 'https://google.com'},
-    {name: 'title 5', description: 'description 5', link: 'https://google.com'},
-  ]
+  const getRepositories = (page = 1, per_page = repos_per_page) => {
+    fetch(`https://api.github.com/users/larrire/repos?page=${page}&per_page=${per_page}`)
+    .then( response => response.json() )
+    .then( data => {
+      setRepositories(data);
+    })
+  }
+
+  useEffect(() => {
+    const first_page = 1;
+    getRepositories(first_page)
+  },[])
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
-
       <ul>
-        { reposiriesArray.map( (repository, index) => <RepositoryItem {...repository} key={index} /> ) }
+        { repositories.map(
+          (repository, index) => <RepositoryItem {...repository} key={index}/>
+        )}
       </ul>
+      <RepositoryPagination 
+        getRepositories={getRepositories} 
+        repos_per_page={repos_per_page}
+        activePage={activePage} setActivePage={setActivePage}
+      />
     </section>
   )
 }
